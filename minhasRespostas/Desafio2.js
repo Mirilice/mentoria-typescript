8632a6d071686ba2f9686a763068668f3e52106b
Maria Alice Oliveira <108701727+Mirilice@users.noreply.github.com>
2022-07-25 20:05:47 -0300

    Add files via upload

---
+++ minhasRespostas/Desafio2.js
@@ -0,0 +1,15 @@
+var pessoa2 = {
+    nome: "roberto",
+    idade: 19,
+    profissao: "Padeiro"
+};
+var pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+var pessoa4 = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+};
