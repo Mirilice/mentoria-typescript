ef01138ffc7ef14fe9d4de6080a189c374ff0926
Mirilice <mariaalicepco10@gmail.com>
2022-07-25 19:57:05 -0300

    Minhas respostas

---
+++ IntroduçãoTypescript-Mirilice/src/Desafio2.ts
@@ -0,0 +1,23 @@
+interface pessoa {
+    nome : string;
+    idade : number | string
+    profissao : string
+}
+
+let pessoa2:pessoa = {
+    nome: "roberto",
+    idade: 19,
+    profissao: "Padeiro",
+}
+
+let pessoa3:pessoa= {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+
+let pessoa4:pessoa = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+}
